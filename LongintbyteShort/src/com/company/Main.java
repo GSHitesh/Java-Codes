3c0c0e8443ac8e7c7cdfb1584c3111960bae5c06
Hitesh Gorantla <saihitesh01@gmail.com>
2021-08-07 15:21:13 +0530

    Byte Primitive Added

--- LongintbyteShort/src/com/company/Main.java
+++ LongintbyteShort/src/com/company/Main.java
@@ -4,10 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int myvalue = 10000;
-        int myMinValue = Integer.MIN_VALUE;
-        int myMaxValue = Integer.MAX_VALUE;
-        System.out.println("Interger Mininmum Value = " + myMinValue);
-        System.out.println("Interger Maximum Value = " + myMaxValue);
+//        int myvalue = 10000;
+//        int myMinValue = Integer.MIN_VALUE;
+//        int myMaxValue = Integer.MAX_VALUE;
+//        System.out.println("Interger Mininmum Value = " + myMinValue);
+//        System.out.println("Interger Maximum Value = " + myMaxValue);
+
+
+        byte myMinValue = Byte.MIN_VALUE;
+        byte myMaxValue = Byte.MAX_VALUE;
+        System.out.println("Byte  Mininmum Value = " + myMinValue);
+        System.out.println("Byte  Maximum Value = " + myMaxValue);
     }
 }
