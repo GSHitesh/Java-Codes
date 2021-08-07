76e91e2acf6b66d14eae6ae9fb0fb107384d3782
Hitesh Gorantla <saihitesh01@gmail.com>
2021-08-07 14:53:18 +0530

    Initial code is added

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public  static void main(String[] args){
+        System.out.println("Hello Hitesh!");
+        int myFisrstnum =10;
+        int mySecindnum =12;
+        int myThirdnum =6;
+        int total = myFisrstnum+mySecindnum+myThirdnum;
+        int myLastone = 1000- total;
+        System.out.println(total);
+        System.out.println(myLastone);
+    }
+        }
