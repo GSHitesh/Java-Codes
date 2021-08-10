cd3bc49c39176781357509d6da802c36bea5358d
Hitesh Gorantla <saihitesh01@gmail.com>
2021-08-10 16:48:11 +0530

    ternary operator

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -3,7 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int result = 1 + 2;
-	System.out.println(result);
+//	boolean result = true;
+//	if(result)
+//        System.out.println("The result is true");
+//    if(result != false)
+//            System.out.println("The result is true");
+//        int result =10;
+//        if(result == 90)
+//            System.out.println("This is an error");
+//        System.out.println("This is correct");
+        boolean car = true;
+        boolean mycar = car ? true : false;
+        if(mycar)
+            System.out.println("This is ryt");
     }
 }
