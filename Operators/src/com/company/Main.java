2ce613958430fc757ab083f131e05072bd3e6b0c
Hitesh Gorantla <saihitesh01@gmail.com>
2021-08-10 16:58:58 +0530

    operator challenge

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -12,9 +12,16 @@ public class Main {
 //        if(result == 90)
 //            System.out.println("This is an error");
 //        System.out.println("This is correct");
-        boolean car = true;
-        boolean mycar = car ? true : false;
-        if(mycar)
-            System.out.println("This is ryt");
+//        boolean car = true;
+//        boolean mycar = car ? true : false;
+//        if(mycar)
+//            System.out.println("This is ryt");
+    //   Challenge
+        double n1 = 20.00d;
+        double n2 = 80.00d;
+        double n3 = ((n1 + n2) *100.00)%40.00d;
+        if(n3 == 0)
+            System.out.println("The remainder is zero ");
+
     }
 }
