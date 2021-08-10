b1edd122e71d8d4024c6b6a7631bc8fa2ac377db
Hitesh Gorantla <saihitesh01@gmail.com>
2021-08-10 15:46:46 +0530

    Challenge pounds to kg

--- LongintbyteShort/src/com/company/Main.java
+++ LongintbyteShort/src/com/company/Main.java
@@ -11,10 +11,18 @@ public class Main {
 //        System.out.println("Interger Maximum Value = " + myMaxValue);
 
 
-        byte myMinValue = Byte.MIN_VALUE;
-        byte myMaxValue = Byte.MAX_VALUE;
-        System.out.println("Byte  Mininmum Value = " + myMinValue);
-        System.out.println("Byte  Maximum Value = " + myMaxValue);
+//        byte myMinValue = Byte.MIN_VALUE;
+//        byte myMaxValue = Byte.MAX_VALUE;
+//        System.out.println("Byte  Mininmum Value = " + myMinValue);
+//        System.out.println("Byte  Maximum Value = " + myMaxValue);
+
+//        float num1 = (float) 254.54545d;
+//        System.out.println("The Float Value: " + num1);\
+
+        //Challenge pounds to kg
+        double pounds = 5545.44d;
+        double kg = pounds* 0.45359237d;
+        System.out.println("The weight in kg is: " + kg);
+
     }
 }
-g
