b296ed72ab82e2341073d53917be47cfdea50df4
Hitesh Gorantla <saihitesh01@gmail.com>
2021-08-12 11:39:06 +0530

    Void function

--- KeywordsandExpressions/src/com/company/Main.java
+++ KeywordsandExpressions/src/com/company/Main.java
@@ -5,12 +5,18 @@ public class Main {
     public static void main(String[] args) {
 //        System.out.println("THis is the one expression, " + "This is another ");
 
-        int num = 100;
-        if(num <100)
-            System.out.println("THis num is less than 100");
-        else if(num == 100)
-            System.out.println("THe num is equal to " + num);
+      calculateScore(250);
 
+    }
+
+    public static void calculateScore(int num) {
+        //int num = 100;
+        if(num <100)
+            System.out.println("This num is less than 100");
+        else if(num >= 100) {
+            System.out.println("The num is equal to " + num);
+            System.out.println("This is another sentence in the line");
+        }
 
     }
 }
