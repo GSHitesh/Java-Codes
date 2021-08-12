c9b0304e6f3ab6f9b1953ddf0a5fa18ac6d8f9fb
Hitesh Gorantla <saihitesh01@gmail.com>
2021-08-12 11:26:51 +0530

    if else if

---
+++ KeywordsandExpressions/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println("THis is the one expression, " + "This is another ");
+
+        int num = 100;
+        if(num <100)
+            System.out.println("THis num is less than 100");
+        else if(num == 100)
+            System.out.println("THe num is equal to " + num);
+
+
+    }
+}
