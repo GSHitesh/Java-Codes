21201fc1227de82377aa425de3d92830fa97828d
Hitesh Gorantla <saihitesh01@gmail.com>
2021-08-26 15:11:07 +0530

    Alternative Method without flag

--- MinMaxChallenge/src/com/Hitesh/Main.java
+++ MinMaxChallenge/src/com/Hitesh/Main.java
@@ -7,7 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int min = 0;
+        //int min = Integer.MAX_VALUE;
         int max = 0;
+//        int max  = Integer.MIN_VALUE;
         boolean first = true;
         while (true){
             System.out.println("Enter the numbers: ");
