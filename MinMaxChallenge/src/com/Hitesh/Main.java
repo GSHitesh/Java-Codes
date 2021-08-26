15719c51627d03bf978060b6684dde886611d036
Hitesh Gorantla <saihitesh01@gmail.com>
2021-08-26 15:09:41 +0530

    Min Max Challenge

---
+++ MinMaxChallenge/src/com/Hitesh/Main.java
@@ -0,0 +1,37 @@
+package com.Hitesh;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int min = 0;
+        int max = 0;
+        boolean first = true;
+        while (true){
+            System.out.println("Enter the numbers: ");
+            boolean isAnInt = scanner.hasNextInt();
+            if(isAnInt){
+                int number = scanner.nextInt();
+                if(first){
+                    first = false;
+                    min = number;
+                    max = number;
+                }
+                if(number > max )
+                    max = number;
+                if (number < min)
+                    min = number;
+
+            }else {
+                break;
+            }
+            scanner.nextLine();
+
+        }
+        System.out.println("The maximum value is: " + max);
+        System.out.println("The minimum value is: "+ min);
+        scanner.close();
+    }
+}
